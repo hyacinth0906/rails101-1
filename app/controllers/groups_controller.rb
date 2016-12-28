7633e002b21996b6aebd5880baee11c1bb1b3ecf
杨茜 <doris_0906@126.com>
2016-12-28 22:11:35 +0800

    style form with bootstrap

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,7 +27,6 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-
     if @group.update(group_params)
        redirect_to groups_path, notice: "Update Success"
     else
