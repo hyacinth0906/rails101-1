bfa56231ee035c00572f412aa0282cb5a55baf49
杨茜 <doris_0906@126.com>
2016-12-29 22:27:57 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -49,8 +49,11 @@ class GroupsController < ApplicationController
 
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
   def group_params
      params.require(:group).permit(:title, :description)
   end
+
 end
